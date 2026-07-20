cbe3d0a95e6d15fa52f737ea79edba82bf106eb1
TJCPM <tjcpm109@gmail.com>
2026-07-20 09:02:11 +0800

    Create sw-v90.js

---
+++ sw-v90.js
@@ -0,0 +1,42 @@
+// sw-v89.js
+const CACHE_NAME = 'tjcpm-cache-v90'; // 💡 升級至 v89，記得同步修改 index.html 內註冊的路徑版本(若有帶版本號)
+
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://tjcpm109.github.io/tjcpm/logo.png',
+  'https://tjcpm109.github.io/tjcpm/APP%20logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' })
+      .then(response => {
+        // 💡 順手把最新抓到的資源寫回快取，離線時才有最新內容可用
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
