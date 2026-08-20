7a3b213e9670d22f1d948ebd8cb3aad2bbf97358
TJCPM <tjcpm109@gmail.com>
2026-08-20 09:39:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2255,7 +2255,7 @@ async function syncProfileAndAccumulatedLeaves() {
       //8/20 document.getElementById(`profileCompensatoryLeave`).textContent = `${data.quota ? data.quota.compLeaveRemainingHours : '—'} 小時`;
 
       document.getElementById(`accumAnnual`).textContent = `${data.quota ? data.quota.specialLeaveUsedHours : annualLeaveUsed}h`;
-      document.getElementById(`accumComp`).textContent = `${compLeaveUsed}h`;
+      document.getElementById(`accumComp`).textContent = `${data.quota ? data.quota.compLeaveUsedHours : compLeaveUsed}h`;
       document.getElementById(`accumSick`).textContent = `${sickLeaveUsed}h`;
       document.getElementById(`accumPersonal`).textContent = `${personalLeaveUsed}h`;
       document.getElementById(`accumOfficial`).textContent = `${officialLeaveUsed}h`;
